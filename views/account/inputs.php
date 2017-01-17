6d563b1d95870d00dc835600b2ed6980552be35f
unknown <>
2017-01-17 21:27:37 +0900

    init commit

---
+++ views/account/inputs.php
@@ -0,0 +1,15 @@
+    <table>
+      <tr>
+        <th>사용자ID</th>
+        <td>
+          <!--escape-->
+          <input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>" />
+        </td>
+      </tr>
+      <tr>
+        <th>패스워드</th>
+        <td>
+          <input type="password" name="password" value="<?php echo $this->escape($password); ?>" />
+        </td>
+      </tr>
+    </table>
